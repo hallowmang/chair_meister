d0964770d1e150642a311d160fc242fd066285c5
Siyoung Choi <102708402+hallowmang@users.noreply.github.com>
2022-12-12 18:18:17 +0900

    rink, header js add
    rink, header js add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 $("body").prepend("<header>")
 $("body").append("<footer>")
 
-$("header").load("./inc.html header")
+$("header").load("./inc.html header",head)
 $("footer").load("./inc.html footer")
+
+let idx =  localStorage.idx || 0;
+
+function head() {
+  $(".nav-bottom>ul>li").eq(idx).addClass("on");
+  $(".nav-bottom>ul>li").click(function () {
+    let idx = $(this).index();
+
+    localStorage.idx = idx;
+
+    /* localStorage.setItem('idx',2);  //idx값 수정
+    localStorage.getItem('idx');  // idx값 가져오기
+
+    localStorage.removeItem('idx'); //삭제
+    localStorage.clear(); //로컬스토리지 모두 삭제 */
+
+  });
+}
