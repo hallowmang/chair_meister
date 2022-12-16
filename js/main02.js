92549be1089badf559e58af0f8b81e068563269d
Siyoung Choi <102708402+hallowmang@users.noreply.github.com>
2022-12-16 18:10:14 +0900

    Re scroll.js
    Re scroll.js

--- js/main02.js
+++ js/main02.js
@@ -1,18 +1,39 @@
-const elTitle = document.querySelector('.main-product-title');
-const elEx = document.querySelector('.main-exhibition-wrap');
+const elTitle = document.querySelector(".main-product-title");
+const elEx = document.querySelector(".main-exhibition-wrap");
 
-onload = function(){
-    const elFooLe = document.querySelector('.footer-top-left');
+onload = function () {
+  const elFooLe = document.querySelector(".footer-top-left");
 
-    window.addEventListener('scroll', function () {
-        /* console.log(window.pageYOffset) */
-        console.log(window.pageYOffset);
-        if (window.pageYOffset > 2344) {
-            elFooLe.style = "opacity: 1;"
-        } else if (window.pageYOffset > 1700) {
-            elEx.style = "opacity: 1;"
-        } else if (window.pageYOffset > 400) {
-            elTitle.style = "opacity: 1;"
-        }
-    })
+  window.addEventListener("scroll", function () {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 2700) {
+      elFooLe.style = "opacity: 1;";
+    } else if (window.pageYOffset > 2000) {
+      elEx.style = "opacity: 1;";
+    } else if (window.pageYOffset > 400) {
+      elTitle.style = "opacity: 1;";
+    }
+  });
+};
+
+/* 사진 스크롤 애니메이션??? */
+/* ::after{
+  width:100%; height:100px;
+  background-color: #555;
+  position: absolute;
+  left:0; top:0; 
+  width:0;
+  animation: 111 ;
 }
+
+@keyframes 111 {
+  0%{
+    left:0; width:0;
+  }
+  50%{width:100%}
+  100%{
+    left:auto;
+    right:0;
+    width:0;
+  }
+} */
